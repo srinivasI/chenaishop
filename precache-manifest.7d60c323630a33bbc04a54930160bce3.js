73d27febdf30b059d8800a234d6f89e9741bf14f
SrinivasI <ippilisrinivas715@gmail.com>
2020-08-05 06:51:16 +0530

    Updates

---
+++ precache-manifest.7d60c323630a33bbc04a54930160bce3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9383ed12ace69b14cad8dfa194f1dbb3",
+    "url": "/chenaishop/index.html"
+  },
+  {
+    "revision": "66078652ec6295260b7e",
+    "url": "/chenaishop/static/css/main.5dc74ef9.chunk.css"
+  },
+  {
+    "revision": "2af5c1d3dd2bd0ee1a28",
+    "url": "/chenaishop/static/js/2.11c49107.chunk.js"
+  },
+  {
+    "revision": "1f80c9c3a5f0eeacf5316e3dcc1a1c4c",
+    "url": "/chenaishop/static/js/2.11c49107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66078652ec6295260b7e",
+    "url": "/chenaishop/static/js/main.d010e7db.chunk.js"
+  },
+  {
+    "revision": "6b0d8854ae1e6685e6a1",
+    "url": "/chenaishop/static/js/runtime-main.93c0d810.js"
+  },
+  {
+    "revision": "c9aafe4fa34265b2453db01acb382cea",
+    "url": "/chenaishop/static/media/background.c9aafe4f.jpg"
+  },
+  {
+    "revision": "891e56ca96149c05da4abf673f4c609d",
+    "url": "/chenaishop/static/media/checkmark.891e56ca.svg"
+  },
+  {
+    "revision": "84a72c4d9ae91e24c7404572db218f3f",
+    "url": "/chenaishop/static/media/loading-bars.84a72c4d.svg"
+  }
+]);
